fe985bf43e83f3d8bbc338462ebaaf7a6c3ed80e
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-10-05 12:34:35 +0200

    new patron working

--- routes/patrons.js
+++ routes/patrons.js
@@ -5,13 +5,58 @@ const Loan = require('../models').Loan;
 const Patron = require('../models').Patron;
 
 /* GET all patrons */
-router.get('/', function(req, res) {
+router.get('/', (req, res) =>{
     Patron.findAll().then((patron) => {
         res.render('all_patrons', {
             patrons: patron,
-            title: 'All Patrons'
+            heading: 'All Patrons'
         });
     });
+});
+
+/* GET add new patron form */
+router.get('/new_patron', (req, res) =>{
+    res.render('new_patron', {
+        heading: 'New Patron',
+    });
+});
+/* ADD new patron - checks for errors carries over values to new rendered page */
+router.post('/new_patron', (req, res, next) =>{
+    Patron.create(req.body)
+        .then((patron) =>{
+            res.redirect("../patrons");
+        }).catch((error) => {
+        if(error.name === "SequelizeValidationError") {
+            res.render("new_patron", {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                address: req.body.address,
+                email: req.body.email,
+                library_id: req.body.library_id,
+                zip_code: req.body.zip_code,
+                errors: error.errors,
+                heading: "New Patron Missing Info"
+            })
+        }else if(error.name === "SequelizeUniqueConstraintError"){
+            res.render("new_patron", {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                address: req.body.address,
+                email: req.body.email,
+                library_id: req.body.library_id,
+                zip_code: req.body.zip_code,
+                errors: error.errors,
+                heading: "That Patron seems to be already in our system"
+            })
+        } else {
+            throw error;
+        }
+
+    }).catch((error) => {
+        res.status(500).send(error);
+        console.log(error)
+    })
+
 });
 
 /* GET details of patron */
@@ -80,5 +125,7 @@ router.post('/:id/update', (req, res) =>{
 
     });
 });
+
+
 
 module.exports = router;
