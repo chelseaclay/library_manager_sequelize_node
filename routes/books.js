5789958d1d2bf5a77f54ecc367eed6cab4d591e8
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-10-09 12:09:41 +0200

    pagination working

--- routes/books.js
+++ routes/books.js
@@ -5,16 +5,62 @@ const moment = require('moment');
 const Book = require('../models').Book;
 const Loan = require('../models').Loan;
 const Patron = require('../models').Patron;
+const amountToShow = 6;
+let pagination = [];
+let currentPage = 1;
+
+function getPagination(books){
+    pagination = [];
+    let numBooks = books.length;
+    let pages = Math.ceil(numBooks/amountToShow);
+    for(let i = 1; i <= pages; i+=1){
+        pagination.push(i);
+    }
+    console.log(numBooks)
+    console.log(pagination)
+    console.log(pages)
+}
+
+function getCurrentPage(req){
+    if(req.query.page <= pagination.length && req.query.page > 0){
+        currentPage = req.query.page;
+    }
+    return currentPage;
+}
+function renderPage(res, req, book, heading){
+    getPagination(book);
+    res.render('all_books', {
+        books: book,
+        heading: heading,
+        currentPage: getCurrentPage(req),
+        pages: pagination
+    });
+}
 
 /* GET all books */
 router.get('/', function(req, res) {
-    Book.findAll({ order: [["first_published", "DESC"]] }).then((book) => {
-        res.render('all_books', {
-            books: book,
-            title: 'Books'
-        });
+    Book.findAll().then((book) => {
+        getPagination(book);
+    }).then(() =>{
+        Book.findAll({
+            order: [["first_published", "DESC"]],
+            limit: amountToShow,
+            offset: amountToShow * (parseInt(req.query.page)-1)})
+            .then((book) =>{
+                console.log(book.length)
+
+                res.render('all_books', {
+                    books: book,
+                    heading: 'Books',
+                    currentPage: getCurrentPage(req),
+                    pages: pagination
+                });
+
+            })
+
     });
-});
+    });
+
 
 /* GET overdue books */
 router.get('/overdue_books', function(req, res) {
@@ -28,12 +74,17 @@ router.get('/overdue_books', function(req, res) {
                 }
             }
         }
-    ]
+    ],
+            limit: amountToShow,
+            offset: amountToShow * (parseInt(req.query.page)-1)
     })
         .then((book) => {
+            getPagination(book);
         res.render('all_books', {
             books: book,
-            title: 'Overdue Books'
+            heading: 'Overdue Books',
+            pages: pagination,
+            currentPage: getCurrentPage(req),
         });
     });
 });
@@ -48,12 +99,17 @@ router.get('/checked_books', function(req, res) {
                 }
             }
         }
-    ]
+    ],
+        limit: amountToShow,
+        offset: amountToShow * (parseInt(req.query.page)-1)
     })
         .then((book) => {
+            getPagination(book);
         res.render('all_books', {
             books: book,
-            title: 'Checked Out Books'
+            heading: 'Checked Out Books',
+            pages: pagination,
+            currentPage: getCurrentPage(req),
         });
     });
 });
