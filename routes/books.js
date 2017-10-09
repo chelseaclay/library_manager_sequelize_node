47989137d83ff3bd573e63e977db0020a5610fa0
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-10-09 13:17:46 +0200

    pagination working loans

--- routes/books.js
+++ routes/books.js
@@ -1,199 +1,209 @@
 "use strict";
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const moment = require('moment');
 const Book = require('../models').Book;
 const Loan = require('../models').Loan;
 const Patron = require('../models').Patron;
-const amountToShow = 6;
-let pagination = [];
-let currentPage = 1;
+const amountToShow = 10;
+let pages = [];
 
-function getPagination(books){
-    pagination = [];
-    let numBooks = books.length;
-    let pages = Math.ceil(numBooks/amountToShow);
-    for(let i = 1; i <= pages; i+=1){
-        pagination.push(i);
+function getPagination(list) {
+    pages = [];
+    let numPages = Math.ceil(list.length / amountToShow);
+    for (let i = 1; i <= numPages; i += 1) {
+        pages.push(i);
     }
-    console.log(numBooks)
-    console.log(pagination)
-    console.log(pages)
-}
-
-function getCurrentPage(req){
-    if(req.query.page <= pagination.length && req.query.page > 0){
-        currentPage = req.query.page;
-    }
-    return currentPage;
-}
-function renderPage(res, req, book, heading){
-    getPagination(book);
-    res.render('all_books', {
-        books: book,
-        heading: heading,
-        currentPage: getCurrentPage(req),
-        pages: pagination
-    });
 }
 
 /* GET all books */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     Book.findAll().then((book) => {
         getPagination(book);
-    }).then(() =>{
+    }).then(() => {
         Book.findAll({
             order: [["first_published", "DESC"]],
             limit: amountToShow,
-            offset: amountToShow * (parseInt(req.query.page)-1)})
-            .then((book) =>{
-                console.log(book.length)
-
+            offset: amountToShow * (parseInt(req.query.page) - 1)
+        })
+            .then((book) => {
                 res.render('all_books', {
                     books: book,
                     heading: 'Books',
-                    currentPage: getCurrentPage(req),
-                    pages: pagination
+                    currentPage: req.query.page,
+                    pages: pages
                 });
-
             })
-
     });
-    });
+});
 
 
 /* GET overdue books */
-router.get('/overdue_books', function(req, res) {
-    Book.findAll(
-        { include: [
-        { model: Loan,
-            where: {
-                returned_on: null,
-                return_by: {
-                $lte: moment().format("YYYY-MM-DD")
+router.get('/overdue_books', function (req, res) {
+    Book.findAll({
+        include: [
+            {
+                model: Loan,
+                where: {
+                    returned_on: null,
+                    return_by: {
+                        $lte: moment().format("YYYY-MM-DD")
+                    }
                 }
             }
-        }
-    ],
-            limit: amountToShow,
-            offset: amountToShow * (parseInt(req.query.page)-1)
-    })
-        .then((book) => {
-            getPagination(book);
-        res.render('all_books', {
-            books: book,
-            heading: 'Overdue Books',
-            pages: pagination,
-            currentPage: getCurrentPage(req),
-        });
+        ]
+    }).then((book) => {
+        getPagination(book);
+    }).then(() => {
+        Book.findAll(
+            {
+                include: [
+                    {
+                        model: Loan,
+                        where: {
+                            returned_on: null,
+                            return_by: {
+                                $lte: moment().format("YYYY-MM-DD")
+                            }
+                        }
+                    }
+                ],
+                limit: amountToShow,
+                offset: amountToShow * (parseInt(req.query.page) - 1)
+            })
+            .then((book) => {
+                res.render('all_books', {
+                    books: book,
+                    heading: 'Overdue Books',
+                    pages: pages,
+                    currentPage: req.query.page,
+                });
+            });
     });
 });
 
 /* GET checked out books */
-router.get('/checked_books', function(req, res) {
-    Book.findAll({ include: [
-        { model: Loan,
-            where: {
-                returned_on: {
-                    $eq: null
+router.get('/checked_books', function (req, res) {
+    Book.findAll({
+        include: [
+            {
+                model: Loan,
+                where: {
+                    returned_on: {
+                        $eq: null
+                    }
                 }
             }
-        }
-    ],
-        limit: amountToShow,
-        offset: amountToShow * (parseInt(req.query.page)-1)
-    })
-        .then((book) => {
-            getPagination(book);
-        res.render('all_books', {
-            books: book,
-            heading: 'Checked Out Books',
-            pages: pagination,
-            currentPage: getCurrentPage(req),
-        });
+        ]
+    }).then((book) => {
+        getPagination(book);
+    }).then(() => {
+        Book.findAll({
+            include: [
+                {
+                    model: Loan,
+                    where: {
+                        returned_on: {
+                            $eq: null
+                        }
+                    }
+                }
+            ],
+            limit: amountToShow,
+            offset: amountToShow * (parseInt(req.query.page) - 1)
+        })
+            .then((book) => {
+                res.render('all_books', {
+                    books: book,
+                    heading: 'Checked Out Books',
+                    pages: pages,
+                    currentPage: req.query.page,
+                });
+            });
     });
 });
 
 /* GET add new book form */
-router.get('/new_book', (req, res) =>{
+router.get('/new_book', (req, res) => {
     res.render('new_book', {
-                heading: 'New Book',
-            });
+        heading: 'New Book',
+    });
 });
 
 /* ADD new book - checks for errors carries over values to new rendered page */
-router.post('/new_book', (req, res, next) =>{
+router.post('/new_book', (req, res, next) => {
     Book.create(req.body)
-        .then(() =>{
+        .then(() => {
             res.redirect("../books");
         }).catch((error) => {
-            if(error.name === "SequelizeValidationError") {
-                res.render("new_book", {
-                    title: req.body.title,
-                    author: req.body.author,
-                    genre: req.body.genre,
-                    first_published: req.body.first_published,
-                    errors: error.errors,
-                    heading: "New Book Missing Info"
-                })
-            }else if(error.name === "SequelizeUniqueConstraintError"){
-                res.render("new_book", {
-                    title: req.body.title,
-                    author: req.body.author,
-                    genre: req.body.genre,
-                    first_published: req.body.first_published,
-                    errors: error.errors,
-                    heading: "That Book seems to be already in our system"
-                })
-            } else {
-                throw error;
-            }
+        if (error.name === "SequelizeValidationError") {
+            res.render("new_book", {
+                title: req.body.title,
+                author: req.body.author,
+                genre: req.body.genre,
+                first_published: req.body.first_published,
+                errors: error.errors,
+                heading: "New Book Missing Info"
+            })
+        } else if (error.name === "SequelizeUniqueConstraintError") {
+            res.render("new_book", {
+                title: req.body.title,
+                author: req.body.author,
+                genre: req.body.genre,
+                first_published: req.body.first_published,
+                errors: error.errors,
+                heading: "That Book seems to be already in our system"
+            })
+        } else {
+            throw error;
+        }
 
     }).catch((error) => {
-            res.status(500).send(error);
-            console.log(error)
-        })
+        res.status(500).send(error);
+        console.log(error)
+    })
 
 });
 /* GET details of book */
-router.get('/:id', (req, res) =>{
+router.get('/:id', (req, res) => {
     const getBook = Book.findOne({
         where: [
-            { id: req.params.id }
+            {id: req.params.id}
         ]
     });
 
     const getLoans = Loan.findAll({
         where: [
-            { book_id: req.params.id }
+            {book_id: req.params.id}
         ],
         include: [{
             model: Patron
         },
             {
-                model: Book}
+                model: Book
+            }
         ],
     });
 
     Promise.all([getBook, getLoans])
         .then(results => {
-        res.render('book_detail', {
-            book: results[0],
-            loans: results[1]
+            res.render('book_detail', {
+                book: results[0],
+                loans: results[1]
+            });
         });
-    });
 });
 
 /* UPDATE details of book */
-router.post('/:id/update', (req, res) =>{
+router.post('/:id/update', (req, res) => {
     const getBook = Book.findOne({
         where: [
-            { id: req.params.id }
+            {id: req.params.id}
         ]
     });
     const getLoans = Loan.findAll({
         where: [
-            { book_id: req.params.id }
+            {book_id: req.params.id}
         ],
         include: [{
             model: Patron
@@ -205,8 +215,8 @@ router.post('/:id/update', (req, res) =>{
             where: [{id: req.params.id}]
         }).then(() => {
             res.redirect('/books');
-        }).catch((error) =>{
-            if(error.name === "SequelizeValidationError") {
+        }).catch((error) => {
+            if (error.name === "SequelizeValidationError") {
                 res.render('book_detail', {
                     book: results[0],
                     loans: results[1],
@@ -220,21 +230,21 @@ router.post('/:id/update', (req, res) =>{
     });
 });
 // Get return of book details
-router.get('/:id/return', (req, res) =>{
+router.get('/:id/return', (req, res) => {
     Loan.findOne({
         where: [
-            { book_id: req.params.id }
+            {book_id: req.params.id}
         ],
         include: [
-            { model: Patron },
-            { model: Book }
+            {model: Patron},
+            {model: Book}
         ]
     }).then((loan) => {
-            res.render('return_book', {
-                loan: loan,
-                returned_on: moment().format("YYYY-MM-DD"),
-                heading: "Return Book"
-            });
+        res.render('return_book', {
+            loan: loan,
+            returned_on: moment().format("YYYY-MM-DD"),
+            heading: "Return Book"
+        });
     });
 });
 
@@ -243,19 +253,19 @@ router.post('/:id/return', (req, res, next) => {
     let errors = [];
     let returned_on = req.body.returned_on;
     let today = moment().format("YYYY-MM-DD");
-    if(!returned_on){
+    if (!returned_on) {
         errors.push('You need to add a date. How about today?')
-    }else if(returned_on > today){
+    } else if (returned_on > today) {
         errors.push('That\'s not a valid date. How about today?');
     }
-    if(errors.length > 0){
+    if (errors.length > 0) {
         Loan.findOne({
             where: [
-                { book_id: req.params.id }
+                {book_id: req.params.id}
             ],
             include: [
-                { model: Patron },
-                { model: Book }
+                {model: Patron},
+                {model: Book}
             ]
         }).then((loan) => {
             res.render('return_book', {
@@ -265,13 +275,12 @@ router.post('/:id/return', (req, res, next) => {
                 errors: errors
             });
         });
-    }else{
-        Loan.update(req.body, { where: [{ book_id: req.params.id }] })
+    } else {
+        Loan.update(req.body, {where: [{book_id: req.params.id}]})
             .then(() => {
                 res.redirect('/loans')
             })
     }
-
 
 
 });
--- routes/loans.js
+++ routes/loans.js
@@ -1,32 +1,49 @@
 "use strict";
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const moment = require('moment');
 const Book = require('../models').Book;
 const Loan = require('../models').Loan;
 const Patron = require('../models').Patron;
+const amountToShow = 10;
+let pages = [];
+
+function getPagination(list) {
+    pages = [];
+    let numPages = Math.ceil(list.length / amountToShow);
+    for (let i = 1; i <= numPages; i += 1) {
+        pages.push(i);
+    }
+}
 
 /* GET all loans */
-router.get('/', function(req, res) {
-    Loan.findAll({
-        include: [
-            { model: Patron },
-            { model: Book }
-        ]
-    }).then((loan) => {
-        res.render('all_loans', {
-            loans: loan,
-            heading: 'Loans'
+router.get('/', function (req, res) {
+    Loan.findAll().then((loan) => {
+        getPagination(loan);
+    }).then(() => {
+        Loan.findAll({
+            include: [
+                {model: Patron},
+                {model: Book}
+            ],
+            limit: amountToShow,
+            offset: amountToShow * (parseInt(req.query.page) - 1)
+        }).then((loan) => {
+            res.render('all_loans', {
+                loans: loan,
+                heading: 'Loans',
+                currentPage: req.query.page,
+                pages: pages
+            });
         });
     });
 });
-
 /* GET checked loans */
-router.get('/checked_loans', function(req, res) {
+router.get('/checked_loans', function (req, res) {
     Loan.findAll({
         include: [
-            { model: Patron },
-            { model: Book }
+            {model: Patron},
+            {model: Book}
         ],
         where: {
             returned_on: {
@@ -34,44 +51,75 @@ router.get('/checked_loans', function(req, res) {
             }
         }
     }).then((loan) => {
-        res.render('all_loans', {
-            loans: loan,
-            heading: 'Checked Loans'
+        getPagination(loan);
+    }).then(() => {
+        Loan.findAll({
+            include: [
+                {model: Patron},
+                {model: Book}
+            ],
+            where: {
+                returned_on: {
+                    $eq: null
+                }
+            },
+            limit: amountToShow,
+            offset: amountToShow * (parseInt(req.query.page) - 1)
+        }).then((loan) => {
+            res.render('all_loans', {
+                loans: loan,
+                heading: 'Checked Loans'
+            });
         });
     });
 });
 
-
 /* GET overdue loans */
-router.get('/overdue_loans', function(req, res) {
+router.get('/overdue_loans', function (req, res) {
     Loan.findAll({
         include: [
-            { model: Patron },
-            { model: Book }
+            {model: Patron},
+            {model: Book}
         ],
         where: {
             returned_on: null,
             return_by: {
                 $lte: moment().format("YYYY-MM-DD")
             }
-        }
+        },
+        limit: amountToShow,
+        offset: amountToShow * (parseInt(req.query.page) - 1)
     }).then((loan) => {
-        res.render('all_loans', {
-            loans: loan,
-            heading: 'Overdue Loans'
+        getPagination(loan);
+    }).then(() => {
+        Loan.findAll({
+            include: [
+                {model: Patron},
+                {model: Book}
+            ],
+            where: {
+                returned_on: null,
+                return_by: {
+                    $lte: moment().format("YYYY-MM-DD")
+                }
+            }
+        }).then((loan) => {
+            res.render('all_loans', {
+                loans: loan,
+                heading: 'Overdue Loans'
+            });
         });
     });
 });
 
-
 /* GET all loans ordered by Book Title */
-router.get('/book', function(req, res) {
+router.get('/book', function (req, res) {
     Loan.findAll({
         include: [
-            { model: Patron },
-            { model: Book}
+            {model: Patron},
+            {model: Book}
         ],
-        order: [ [ Book, 'title' ] ]
+        order: [[Book, 'title']]
     }).then((loan) => {
         res.render('all_loans', {
             loans: loan,
@@ -80,13 +128,13 @@ router.get('/book', function(req, res) {
     });
 });
 /* GET all loans ordered by Patron */
-router.get('/patron', function(req, res) {
+router.get('/patron', function (req, res) {
     Loan.findAll({
         include: [
-            { model: Patron },
-            { model: Book }
+            {model: Patron},
+            {model: Book}
         ],
-        order: [ [ Patron, 'first_name' ] ]
+        order: [[Patron, 'first_name']]
     }).then((loan) => {
         res.render('all_loans', {
             loans: loan,
@@ -95,13 +143,13 @@ router.get('/patron', function(req, res) {
     });
 });
 /* GET all loans ordered by Loaned on */
-router.get('/loanedOn', function(req, res) {
+router.get('/loanedOn', function (req, res) {
     Loan.findAll({
         include: [
-            { model: Patron },
-            { model: Book }
+            {model: Patron},
+            {model: Book}
         ],
-        order: [ ['loaned_on' ] ]
+        order: [['loaned_on']]
     }).then((loan) => {
         res.render('all_loans', {
             loans: loan,
@@ -110,13 +158,13 @@ router.get('/loanedOn', function(req, res) {
     });
 });
 /* GET all loans ordered by Return by */
-router.get('/returnBy', function(req, res) {
+router.get('/returnBy', function (req, res) {
     Loan.findAll({
         include: [
-            { model: Patron },
-            { model: Book }
+            {model: Patron},
+            {model: Book}
         ],
-        order: [ ['return_by' ] ]
+        order: [['return_by']]
     }).then((loan) => {
         res.render('all_loans', {
             loans: loan,
@@ -125,13 +173,13 @@ router.get('/returnBy', function(req, res) {
     });
 });
 /* GET all loans ordered by Returned on */
-router.get('/returned', function(req, res) {
+router.get('/returned', function (req, res) {
     Loan.findAll({
         include: [
-            { model: Patron },
-            { model: Book }
+            {model: Patron},
+            {model: Book}
         ],
-        order: [ [ 'returned_on' ] ]
+        order: [['returned_on']]
     }).then((loan) => {
         res.render('all_loans', {
             loans: loan,
@@ -142,7 +190,7 @@ router.get('/returned', function(req, res) {
 
 
 // GET new loan form
-router.get('/new_loan', function(req, res, next) {
+router.get('/new_loan', function (req, res, next) {
     const getBooks = Book.findAll();
     const getPatrons = Patron.findAll();
     const getLoans = Loan.findAll({
@@ -158,14 +206,14 @@ router.get('/new_loan', function(req, res, next) {
             let today = moment().format("YYYY-MM-DD");
             let returnBy = moment(new Date().setDate(new Date().getDate() + 7)).format("YYYY-MM-DD");
             let loanedBooks = [];
-            let availableBooks =[];
+            let availableBooks = [];
             let books = results[0];
             let loans = results[1];
             let allPatrons = results[2];
-            loans.forEach(function(loan){
+            loans.forEach(function (loan) {
                 loanedBooks.push(loan.book_id)
             });
-            books.forEach(function(book) {
+            books.forEach(function (book) {
                 if (loanedBooks.indexOf(book.id) < 0) {
                     availableBooks.push(book);
                 }
@@ -182,7 +230,7 @@ router.get('/new_loan', function(req, res, next) {
         });
 });
 // ADD new loan to the database
-router.post('/new_loan', function(req, res, next) {
+router.post('/new_loan', function (req, res, next) {
 
     Loan.create(req.body)
         .then(() => {
