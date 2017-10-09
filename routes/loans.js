3dfca46b12c10d5fb4ca8849b5d0ace7a59ab5ba
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-10-09 09:52:18 +0200

    loan validation working

--- routes/loans.js
+++ routes/loans.js
@@ -20,6 +20,7 @@ router.get('/', function(req, res) {
         });
     });
 });
+
 /* GET checked loans */
 router.get('/checked_loans', function(req, res) {
     Loan.findAll({
@@ -61,6 +62,85 @@ router.get('/overdue_loans', function(req, res) {
         });
     });
 });
+
+
+/* GET all loans ordered by Book Title */
+router.get('/book', function(req, res) {
+    Loan.findAll({
+        include: [
+            { model: Patron },
+            { model: Book}
+        ],
+        order: [ [ Book, 'title' ] ]
+    }).then((loan) => {
+        res.render('all_loans', {
+            loans: loan,
+            heading: 'Loans'
+        });
+    });
+});
+/* GET all loans ordered by Patron */
+router.get('/patron', function(req, res) {
+    Loan.findAll({
+        include: [
+            { model: Patron },
+            { model: Book }
+        ],
+        order: [ [ Patron, 'first_name' ] ]
+    }).then((loan) => {
+        res.render('all_loans', {
+            loans: loan,
+            heading: 'Loans'
+        });
+    });
+});
+/* GET all loans ordered by Loaned on */
+router.get('/loanedOn', function(req, res) {
+    Loan.findAll({
+        include: [
+            { model: Patron },
+            { model: Book }
+        ],
+        order: [ ['loaned_on' ] ]
+    }).then((loan) => {
+        res.render('all_loans', {
+            loans: loan,
+            heading: 'Loans'
+        });
+    });
+});
+/* GET all loans ordered by Return by */
+router.get('/returnBy', function(req, res) {
+    Loan.findAll({
+        include: [
+            { model: Patron },
+            { model: Book }
+        ],
+        order: [ ['return_by' ] ]
+    }).then((loan) => {
+        res.render('all_loans', {
+            loans: loan,
+            heading: 'Loans'
+        });
+    });
+});
+/* GET all loans ordered by Returned on */
+router.get('/returned', function(req, res) {
+    Loan.findAll({
+        include: [
+            { model: Patron },
+            { model: Book }
+        ],
+        order: [ [ 'returned_on' ] ]
+    }).then((loan) => {
+        res.render('all_loans', {
+            loans: loan,
+            heading: 'Loans'
+        });
+    });
+});
+
+
 // GET new loan form
 router.get('/new_loan', function(req, res, next) {
     const getBooks = Book.findAll();
@@ -103,64 +183,52 @@ router.get('/new_loan', function(req, res, next) {
 });
 // ADD new loan to the database
 router.post('/new_loan', function(req, res, next) {
-    const getBooks = Book.findAll();
-    const getPatrons = Patron.findAll();
-    const getLoans = Loan.findAll({
-        attribute: ['book_id'],
-        where: {
-            returned_on: null
-        }
-    });
 
-    //once all info has been got sort books to only show ones that haven't been loaned
-    Promise.all([getBooks, getLoans, getPatrons])
-        .then(results => {
-            let today = moment().format("YYYY-MM-DD");
-            let returnBy = moment(new Date().setDate(new Date().getDate() + 7)).format("YYYY-MM-DD");
-            let loanedBooks = [];
-            let availableBooks = [];
-            let books = results[0];
-            let loans = results[1];
-            let allPatrons = results[2];
-            loans.forEach(function (loan) {
-                loanedBooks.push(loan.book_id)
-            });
-            books.forEach(function (book) {
-                if (loanedBooks.indexOf(book.id) < 0) {
-                    availableBooks.push(book);
-                }
-            });
-
-            res.render('new_loan', {
-                loans: loans,
-                books: availableBooks,
-                patrons: allPatrons,
-                loaned_on: today,
-                return_by: returnBy,
-                heading: 'New Loan'
-            });
-        }).then(results => {
-        Loan.create(req.body)
-            .then(() => {
-                res.redirect('/loans');
-            })
-            .catch((error, loans, availableBooks, allPatrons, today, returnBy) =>{
-                if (error.name === 'SequelizeValidationError') {
+    Loan.create(req.body)
+        .then(() => {
+            res.redirect('/loans');
+        }).catch((error) => {
+        const getBooks = Book.findAll();
+        const getPatrons = Patron.findAll();
+        const getLoans = Loan.findAll({
+            attribute: ['book_id'],
+            where: {
+                returned_on: null
+            }
+        });
+        Promise.all([getBooks, getLoans, getPatrons])
+            .then(results => {
+                if (error.name) {
+                    let today = moment().format("YYYY-MM-DD");
+                    let returnBy = moment(new Date().setDate(new Date().getDate() + 7)).format("YYYY-MM-DD");
+                    let loanedBooks = [];
+                    let availableBooks = [];
+                    let books = results[0];
+                    let loans = results[1];
+                    let allPatrons = results[2];
+                    loans.forEach(function (loan) {
+                        loanedBooks.push(loan.book_id)
+                    });
+                    books.forEach(function (book) {
+                        if (loanedBooks.indexOf(book.id) < 0) {
+                            availableBooks.push(book);
+                        }
+                    });
                     res.render('new_loan', {
-                        errors: error.errors,
                         loans: loans,
                         books: availableBooks,
                         patrons: allPatrons,
                         loaned_on: today,
                         return_by: returnBy,
-                        heading: "big problems"
+                        heading: 'There seems to have been an error',
+                        errors: error.errors
                     });
+                } else {
+                    res.status(500).send(error);
                 }
+
+
             })
-            .catch(function (error) {
-                console.log('Error: ' + error);
-                res.status(500).send(error);
-            });
     });
 });
 
